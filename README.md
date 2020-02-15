b70c248bf3d204dc580f32be2271245fcc1585a3
Werner Donné <werner.donne@pincette.biz>
2020-02-15 17:21:36 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,36 @@
+# JSON Event Sourcing AWS Lambda Trigger
+
+This microservice calls AWS Lambdas with messages from Kafka topics. The topics should use the [serialisation of JSON Event Sourcing](https://www.javadoc.io/static/net.pincette/pincette-jes-util/1.1.3/net/pincette/jes/util/JsonSerde.html).
+
+## Configuration
+
+The configuration is managed by the 
+[Lightbend Config package](https://github.com/lightbend/config). By default it will try to load ```conf/application.conf```. An alternative configuration may be loaded by adding ```-Dconfig.resource=myconfig.conf```, where the file is also supposed to be in the ```conf``` directory. The following entries are available.
+
+|Entry|Description|
+|---|---|
+|logLevel|The level of the logger. The default value is "INFO".|
+|logTopic|The name of the log Kafka topic. This entry is mandatory.|
+|kafka|All Kafka settings come below this entry. So for example, the setting ```bootstrap.servers``` would go to the entry ```kafka.bootstrap.servers```.|
+|kafka.num.stream.threads|The number of worker threads per instance.|
+|kafka.replication.factor|When using Confluent Cloud this should be 3.|
+|triggers|This is an object where the keys are Kafka topics and the values are arrays of AWS ARNs denoting lambda functions.|
+
+## Building and Running
+
+You can build the tool with ```mvn clean package```. This will produce a self-contained JAR-file in the ```target``` directory with the form ```pincette-jes-lambda-<version>-jar-with-dependencies.jar```. You can launch this JAR with ```java -jar```, without further options.
+
+The total number of threads across all the instances should not exceed the number of partitions for the Kafka topics. Additional threads will be idle.
+
+You can run the JVM with the option ```-mx128m```.
+
+## Docker
+
+Docker images can be found at [https://hub.docker.com/repository/docker/jsoneventsourcing/pincette-jes-lambda](https://hub.docker.com/repository/docker/jsoneventsourcing/pincette-jes-lambda). You should add a configuration layer with a Docker file that looks like this:
+
+```
+FROM registry.hub.docker.com/jsoneventsourcing/pincette-jes-lambda:<version>
+COPY conf/tst.conf /conf/application.conf
+```
+
+So wherever your configuration file comes from, it should always end up at ```/conf/application.conf```.
---
+++ src/main/java/net/pincette/jes/lambda/Trigger.java
@@ -0,0 +1,124 @@
+package net.pincette.jes.lambda;
+
+import static java.lang.System.exit;
+import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.SEVERE;
+import static java.util.logging.Level.parse;
+import static java.util.logging.Logger.getLogger;
+import static net.pincette.jes.elastic.Logging.log;
+import static net.pincette.jes.util.Configuration.loadDefault;
+import static net.pincette.jes.util.Kafka.createReliableProducer;
+import static net.pincette.jes.util.Kafka.fromConfig;
+import static net.pincette.jes.util.Streams.start;
+import static net.pincette.json.JsonUtil.string;
+import static net.pincette.util.Util.tryToGetSilent;
+import static software.amazon.awssdk.core.SdkBytes.fromUtf8String;
+import static software.amazon.awssdk.services.lambda.LambdaAsyncClient.create;
+import static software.amazon.awssdk.services.lambda.model.InvocationType.EVENT;
+import static software.amazon.awssdk.services.lambda.model.InvokeRequest.builder;
+
+import com.typesafe.config.Config;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.JsonObject;
+import net.pincette.function.SideEffect;
+import net.pincette.jes.util.JsonSerializer;
+import net.pincette.jes.util.Streams;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import software.amazon.awssdk.services.lambda.LambdaAsyncClient;
+
+/**
+ * This program listens to configured Kafka topics for messages of the type <code>JsonObject</code>
+ * and calls a configured array of lambdas with them. The messages are sent as events and the
+ * lambdas are called asychronously.
+ *
+ * @author Werner Donn\u00e9
+ */
+public class Trigger {
+  private static final String ENVIRONMENT = "environment";
+  private static final String KAFKA = "kafka";
+  private static final String LOG_LEVEL = "logLevel";
+  private static final String LOG_TOPIC = "logTopic";
+  private static final String VERSION = "1.0";
+
+  private static void callLambda(
+      final String arn,
+      final JsonObject message,
+      final LambdaAsyncClient client,
+      final Logger logger) {
+    client
+        .invoke(
+            builder()
+                .functionName(arn)
+                .invocationType(EVENT)
+                .payload(fromUtf8String(string(message)))
+                .build())
+        .thenAccept(
+            response ->
+                Optional.ofNullable(response.functionError())
+                    .ifPresent(
+                        error ->
+                            logger.log(
+                                SEVERE,
+                                "Calling lambda {0} returned error {1} with payload {2}",
+                                new Object[] {arn, error, response.payload().asUtf8String()})));
+  }
+
+  private static void connect(
+      final String topic,
+      final List<String> arns,
+      final LambdaAsyncClient client,
+      final StreamsBuilder builder,
+      final Logger logger) {
+    final KStream<String, JsonObject> stream = builder.stream(topic);
+
+    stream.mapValues(
+        v ->
+            SideEffect.<Boolean>run(() -> arns.forEach(arn -> callLambda(arn, v, client, logger)))
+                .andThenGet(() -> true));
+  }
+
+  static String env(final Config config) {
+    return tryToGetSilent(() -> config.getString(ENVIRONMENT)).orElse("dev");
+  }
+
+  private static Level logLevel(final Config config) {
+    return parse(tryToGetSilent(() -> config.getString(LOG_LEVEL)).orElse("INFO"));
+  }
+
+  public static void main(final String[] args) {
+    final StreamsBuilder builder = new StreamsBuilder();
+    final Config config = loadDefault();
+    final Logger logger = getLogger("pincette-jes-lambda");
+    final Config triggers = config.getConfig("triggers");
+
+    logger.setLevel(logLevel(config));
+
+    try (final LambdaAsyncClient client = create();
+        final KafkaProducer<String, JsonObject> producer =
+            createReliableProducer(
+                fromConfig(config, KAFKA), new StringSerializer(), new JsonSerializer())) {
+      log(logger, logger.getLevel(), VERSION, env(config), producer, config.getString(LOG_TOPIC));
+
+      triggers
+          .entrySet()
+          .forEach(
+              e ->
+                  connect(e.getKey(), triggers.getStringList(e.getKey()), client, builder, logger));
+
+      final Topology topology = builder.build();
+
+      logger.log(INFO, "Topology:\n\n {0}", topology.describe());
+
+      if (!start(topology, Streams.fromConfig(config, KAFKA))) {
+        exit(1);
+      }
+    }
+  }
+}
